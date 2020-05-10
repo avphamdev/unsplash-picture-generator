81f934f480472b83dcfbd1aaf585846ddd6f2de3
An Pham <sadandboujee4474@gmail.com>
2020-05-10 18:08:23 +0700

    First commit

---
+++ BeautifulPictureGenerator/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace BeautifulPictureGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Save = new System.Windows.Forms.Button();
+            this.Generate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MintCream;
+            this.panel1.Controls.Add(this.Save);
+            this.panel1.Controls.Add(this.Generate);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 380);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(917, 129);
+            this.panel1.TabIndex = 0;
+            // 
+            // Save
+            // 
+            this.Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Save.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Save.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.Location = new System.Drawing.Point(130, 32);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(182, 37);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save the picture";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Generate
+            // 
+            this.Generate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.Generate.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.Generate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Generate.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Generate.Location = new System.Drawing.Point(604, 32);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(125, 37);
+            this.Generate.TabIndex = 1;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = false;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(318, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 37);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(917, 380);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MintCream;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(75, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(766, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập một keyword bất kỳ vào textbox bên dưới để search hình ảnh";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.MintCream;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(917, 380);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 509);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Picture Generator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BeautifulPictureGenerator/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Windows.Forms;
+
+
+namespace BeautifulPictureGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Generate_Click(object sender, EventArgs e)
+        {
+            label1.Text = null;
+            if (string.IsNullOrEmpty(textBox1.Text)) MessageBox.Show("Bạn chưa nhập từ khóa, vui lòng thử lại.");
+            else
+            {
+                HttpClient client = new HttpClient();
+
+                string url = $"https://api.unsplash.com/photos/random/?client_id=EnterYourApiHere&query={textBox1.Text}";
+
+                try
+                {
+                    string response = await client.GetStringAsync(url);
+
+                    var jsonObj = (JObject)JsonConvert.DeserializeObject(response);
+
+                    string image = (string)jsonObj["urls"]["regular"];
+
+                    pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+                    pictureBox1.LoadAsync(image);
+
+                    folderBrowserDialog1.Tag = (string)jsonObj["alt_description"];
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Từ khóa bạn tìm kiếm không tồn tại, vui lòng thử lại.");
+                    MessageBox.Show("Hoặc API Call đã đạt giới hạn, vui lòng thử lại sau.");
+                }
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null) MessageBox.Show("Bạn chưa tìm kiếm tấm ảnh nào, vui lòng thử lại.");
+            else
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(folderBrowserDialog1.SelectedPath + (string)folderBrowserDialog1.Tag + ".jpg");
+                    MessageBox.Show("Ảnh của bạn đã được lưu.");
+                }
+            }
+        }
+    }
+}
